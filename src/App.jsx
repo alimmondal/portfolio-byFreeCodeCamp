ad9918ec76852fa8093c64f3f50078341e0f21a5
alimmondal <76857187+alimmondal@users.noreply.github.com>
2022-04-01 18:19:24 +0600

    updated

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { Route, Routes } from 'react-router-dom';
 import Home from './components/Home';
 import About from './components/About';
-// import Contact from './components/Contact';
+import Contact from './components/Contact';
 import Layout from './components/Layout';
 import './App.scss';
 
@@ -13,7 +13,7 @@ function App() {
         <Route path="/" element={<Layout />}>
           <Route index element={<Home />} />
           <Route path="about" element={<About />} />
-          {/* <Route path="/contact" element={<Contact />} /> */}
+          <Route path="/contact" element={<Contact />} />
         </Route>
       </Routes>
     </>
